64cb653efaac6309f5bbf3c85f1175a38aa328e7
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-02-28 22:29:29 -0500

    added conditions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,41 @@
-// this creates a function named "fight"
-
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log("This logs a string, good for leaving yourself a message");
-console.log(10+10);
-console.log("Our robot's name is " + playerName);
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-function fight() {
-    window.alert("The fight has begun!");
-}
+var fight = function() {
+    //alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
 
-// fight();
+    //subtract the value of 'playerAttack' from the value of 'enemyHealth'
+    enemyHealth = enemyHealth - playerAttack;
+    //log a resulting message to the console so we know that it worked
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + " health remaining."
+    );
+
+    //check player health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+
+    //check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+    //subtract the value of 'enemyAttack' from the value of 'playerHealth'
+    playerHealth = playerHealth - enemyAttack;
+    //log a resulting message to the console so we know it worked
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+};
+
+fight();
